1a7d5e56fb2bff1c5d8061d4e666f1506752e4a6
grasool <mr.ghulamrasool@gmail.com>
2024-02-05 12:29:09 -0500

    update

--- local-llms-langchain.py
+++ local-llms-langchain.py
@@ -1,21 +1,14 @@
-#CreateQAs-LLM.py
 
 # This script is testing local llms using LM Studio and langchain_openai
 
-#from langchain.chains import RetrievalQA
-#from langchain.document_loaders import CSVLoader
-#from langchain.indexes import VectorstoreIndexCreator
-#from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.prompts import ChatPromptTemplate
-
-
 from langchain_openai import ChatOpenAI
+
 chat = ChatOpenAI(temperature=0.0, base_url="http://localhost:1234/v1", api_key="not-needed")
 
 
 template_string = """Translate the text that is delimited by triple backticks \
-into a style that is {style}. text: ```{text}```
-"""
+into a style that is {style}. text: ```{text}``` """
 
 customer_style = """American English in a calm and respectful tone """
 
@@ -26,7 +19,6 @@ cleaning up me kitchen. I need yer help right now, matey! """
 prompt_template = ChatPromptTemplate.from_template(template_string)
 
 
-
 customer_messages = prompt_template.format_messages(
                     style=customer_style,
                     text=customer_email)
@@ -34,7 +26,6 @@ customer_messages = prompt_template.format_messages(
 print(customer_messages)
 
 customer_response = chat(customer_messages)
-
 
 print(customer_response.content)
 
