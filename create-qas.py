8492b8d36d2987ce90f37b3ddc9c211673236514
grasool <mr.ghulamrasool@gmail.com>
2024-02-05 13:14:50 -0500

    This is a test file

--- create-qas.py
+++ create-qas.py
@@ -5,5 +5,20 @@
 from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
-chat = ChatOpenAI(temperature=0.0, base_url="http://localhost:1234/v1", api_key="not-needed")
+llm_model = ChatOpenAI(temperature=0.0, base_url="http://localhost:1234/v1", api_key="not-needed")
 
+
+with open('Book-In-Text.txt', 'r') as file:
+    data = file.read()
+#print(data)
+
+
+from langchain.evaluation.qa import QAGenerateChain
+
+# example_gen_chain = QAGenerateChain.from_llm(llm=llm_model)
+# new_examples = example_gen_chain.apply_and_parse(
+#     [{"doc": t} for t in data[:5]]
+# )
+
+print([{"doc": t} for t in data[:5]])
+print()
